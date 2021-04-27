6568197e5858ea2a3fb76702e79067700271912e
LonnonjamesD <LonnonjamesD@gmail.com>
2021-04-26 20:36:09 -0400

    Big

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,16 @@
 import React from 'react';
+import logo from './logo.svg';
+
+function Heading({title}: {title: string}) {
+	return <h1>{title}</h1>
+}
+
+function App() {
+	return (
+		<div>
+			<Heading title="Hi"></Heading>
+		</div>
+	);
+}
+
+export default App;
